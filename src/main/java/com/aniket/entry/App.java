1a86fef47819f270a5f5b0bd4fcec135f710ca5b
ANIKET MISHRA <ANIKET MISHRA@192.168.0.105>
2019-01-23 00:15:59 +0530

    First Maven Proj Create

---
+++ src/main/java/com/aniket/entry/App.java
@@ -0,0 +1,34 @@
+package com.aniket.entry;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.aniket.model.Student;
+
+public class App {
+
+	public static void main(String[] args) {
+		Student student1 = new Student();
+		student1.setId(101);
+		student1.setName("Aniket");
+		student1.setCity("Noida");
+		
+		Student student2 = new Student();
+		student2.setId(102);
+		student2.setName("Narendra");
+		student2.setCity("");
+		List<Student> students=Arrays.asList(student1,student2);
+		//--Feature of java 8
+		students.forEach(student->{
+			
+			if(StringUtils.isBlank(student.getCity())) {
+				System.out.println("City cannot be blank.");
+			}
+		});
+		
+		
+	}
+
+}
